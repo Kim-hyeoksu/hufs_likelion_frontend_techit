c9db0c5a2c6532f69aa04b25415f9c9bb0a1df3b
kidstone <hyuksu0308@naver.com>
2023-07-18 00:25:08 +0900

    react-#6.4Cleanup

--- react-for-beginners-nomad/src/App.js
+++ react-for-beginners-nomad/src/App.js
@@ -1,10 +1,51 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  function byFn() {
+    console.log('bye :(');
+  }
+  function hiFn() {
+    console.log('created :)');
+    return byFn;
+  }
+  useEffect(()=>{
+    console.log('hi');
+    return () => console.log('bye');
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  const onClickShowing = () => setShowing(prev => !prev);
+  console.log('all the time')
+  useEffect(() => {
+    console.log('only one')
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log('search for', keyword);
+    }
+  }, [keyword]);
   return (
     <div className="App">
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <input 
+      value={keyword} 
+      onChange={onChange} 
+      type="text" 
+      placeholder="Search here,,"/>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"counter"} onClick={onClick}/>
+      <div>
+        {showing ? <Hello/> : null}
+        <button onClick={onClickShowing}>{showing ? "Hide" : "Show"}</button>
+      </div>
     </div>
   );
 }
